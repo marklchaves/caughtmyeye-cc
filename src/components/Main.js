7da80fa8d205620657401c049a5c360b5b87146b
mark l chaves <mark@marklchaves.com>
2020-03-14 19:36:16 +0800

    Fix for form submission

--- src/components/Main.js
+++ src/components/Main.js
@@ -101,7 +101,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact" netlify>
+          <form name="contact" method="POST" data-netlify="true">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
