b22dddbb237bcceaccbe6fc2209ce85d257af23a
mark l chaves <mark@marklchaves.com>
2020-03-14 20:37:19 +0800

    Ditching Netlify forms, switched to Formspree

--- src/components/Main.js
+++ src/components/Main.js
@@ -101,7 +101,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+          <form name="contact" method="post" action="https://formspree.io/xyypknql">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
