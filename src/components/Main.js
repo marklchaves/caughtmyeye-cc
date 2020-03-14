0101147509869b4e38d1ab39d96dcc7bd3a833ad
mark l chaves <mark@marklchaves.com>
2020-03-14 13:48:14 +0800

    Customising and adding content

--- src/components/Main.js
+++ src/components/Main.js
@@ -29,28 +29,11 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Intro</h2>
-          <span className="image main">
-            <img src={pic01} alt="" />
-          </span>
           <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
+            Freelancer based in Bali, Indonesia.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
+            Photography, writing, and web development.
           </p>
           {close}
         </article>
@@ -63,23 +46,23 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Work</h2>
-          <span className="image main">
-            <img src={pic02} alt="" />
-          </span>
           <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
+            Photography: Loop Images UK, Airbnb Plus Bali, and BaliStreetPhotographer.com.
           </p>
           <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
+            Writing: AirAsia Travel 3Sixty Magazine, Asian Geographic, Medium, DEV.to, Vimpt, Editorr, and Grryo.
+          </p>
+          <p>
+            Websites: EvePersak.com, ScalingStudentSuccess.org, and Accuvalent.com. 
+          </p>
+          <p>
+            Apps: IniBali.caughtmyeye.cc, Hope.caughtmyeye.cc, Shorties.caughtmyeye.cc. 
+          </p>
+          <p>
+            WordPress plugins: Gallery Image Captions, Hide and Seek Header, and Ko-fi Button.
+          </p>
+          <p>
+            Code samples on GitHub and CodePen.
           </p>
           {close}
         </article>
@@ -92,17 +75,11 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">About</h2>
-          <span className="image main">
-            <img src={pic03} alt="" />
-          </span>
           <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
+            Before freelancing reelancer, I slung code and managed web teams for companies such as IBM, Oracle, and Sony PlayStation. I have an undergraduate degree in software engineering from The Ohio State University. I have a masters in computer science from The American University in Washington, D.C.
+          </p>
+          <p>
+            See my profiles on <a href="https://balistreetphotographer.com/about/">BaliStreetPhotographer.com</a>, <a href="https://dev.to/marklchaves/">DEV.to</a>, and <a href="https://www.quora.com/profile/Mark-Chaves/">Quora</a>.
           </p>
           {close}
         </article>
@@ -131,9 +108,6 @@ class Main extends React.Component {
             <ul className="actions">
               <li>
                 <input type="submit" value="Send Message" className="special" />
-              </li>
-              <li>
-                <input type="reset" value="Reset" />
               </li>
             </ul>
           </form>
