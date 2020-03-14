f005d8abd9723ef171195a61e2e9b0bb5c253c59
mark l chaves <mark@marklchaves.com>
2020-03-14 20:27:17 +0800

    Very last try to fix form

--- src/components/Main.js
+++ src/components/Main.js
@@ -101,7 +101,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact" method="POST" data-netlify="true">
+          <form name="contact" netlify netlify-honeypot="bot-field" hidden>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
