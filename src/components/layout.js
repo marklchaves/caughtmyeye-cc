fe9096cef91652573ac516beabee6ea27cfca624
mark l chaves <mark@marklchaves.com>
2020-04-02 08:41:41 +0800

    OG meta

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,11 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import '../assets/scss/main.scss'
 
+import siteImg from "../images/cme-logo-black-clear-512x512.png"
+
+const locale = 'en_US'
+const siteUrl = 'https://www.caughtmyeye.cc'
+
 const Layout = ({ children, location }) => {
 
   let content;
@@ -41,11 +46,22 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: 'to see rather than look - photography writing web development - bali indonesia' },
+              { name: 'keywords', content: 'photography, interior, photo essay, writing, webdev, web development, wordpress, gatsby, jekyll, php, javascript, html, css, freelance, freelancer, bali' },
+              { property: 'og:locale', content: locale },
+              { property: 'og:site_name', content: data.site.siteMetadata.title },
+              { property: 'og:url', content: siteUrl },
+              { property: 'og:image', content: siteImg },
+              { property: 'og:image:width', content: '512' },
+              { property: 'og:image:height', content: '512' },
+              { property: 'twitter:site', content: '@marklchaves' },
+              { property: 'twitter:title', content: data.site.siteMetadata.title },
+              { property: 'twitter:url', content: siteUrl },
+              { property: 'twitter:card', content: 'summary'},
             ]}
           >
-            <html lang="en" />
+            <link rel="canonical" href={siteUrl} />
+            <html lang={locale} />
           </Helmet>
           {content}
         </>
