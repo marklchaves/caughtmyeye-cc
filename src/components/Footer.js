678d5537c644a90febf5a9b53d31e75856f07577
mark l chaves <mark@marklchaves.com>
2020-04-13 07:39:05 +0800

    Point footer to cme dev
    Not to itself.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ let currYear = new Date().getFullYear();
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; {currYear} <a href="https://caughtmyeye.cc">caught my eye dev</a>. Built with <a href="https://www.gatsbyjs.org/">Gatsby.js</a>. Based on Dimension by <a href="https://html5up.net/">HTML5 UP</a>.</p>
+        <p className="copyright">&copy; {currYear} <a href="https://www.caughtmyeye.dev/">caught my eye dev</a>. Built with <a href="https://www.gatsbyjs.org/">Gatsby.js</a>. Based on Dimension by <a href="https://html5up.net/">HTML5 UP</a>.</p>
     </footer>
 )
 
